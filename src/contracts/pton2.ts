ed693661f11e0b1caa18289d3178e88ddb784745
elsvv <elis.alech900@gmail.com>
2024-11-04 12:30:51 +0300

    fix pton2 code cell

--- src/contracts/pton2.ts
+++ src/contracts/pton2.ts
@@ -3,9 +3,12 @@ import { packStateinit } from "../utils";
 
 export const PTON2_MAINNET_ADDR = Address.parse("EQBnGWMCf3-FZZq1W4IWcWiGAc3PHuZ0_H-7sad2oY00o83S");
 
-const PTON2_JETTON_WALLET_CODE = Cell.fromBase64(
-  "b5ee9c7201010101002300084202cd88e6f3c2a9cf01bb003a2837ec0d92c19685ed1dbfffd94a545dcfdf0a14d9"
-);
+const PTON2_JETTON_WALLET_CODE = Cell.fromBoc(
+  Buffer.from(
+    "b5ee9c7201010101002300084202cd88e6f3c2a9cf01bb003a2837ec0d92c19685ed1dbfffd94a545dcfdf0a14d9",
+    "hex"
+  )
+)[0];
 
 export function calculatePTon2JettonWalletStateinit(ownerAddressHash: Buffer): Cell {
   const data = beginCell()
